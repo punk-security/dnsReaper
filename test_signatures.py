f2798ce057606b43778785618bc5f051b7f39a85
Simon Gurney <simon.gurney@punksecurity.co.uk>
2022-07-19 16:49:36 +0100

    feat: add validation for signatures

--- test_signatures.py
+++
@@ -1,9 +0,0 @@
-from pyclbr import Function
-import signatures
-
-signatures = [getattr(signatures, signature) for signature in signatures.__all__]
-
-for signature in signatures:
-    assert callable(getattr(signature, "potential"))
-    assert callable(getattr(signature, "check"))
-    assert type(getattr(signature, "INFO")) is str
