b4fed38151eb3d1f481a8d1ee6490c7c1ae83615
Simon Gurney <simon.gurney@punksecurity.co.uk>
2022-07-21 16:25:08 +0100

    feat: add routine for NXDOMAIN

--- signatures/generic/CNAME.py
+++ signatures/generic/CNAME.py
@@ -9,3 +9,23 @@ def match(domain: Domain, strings) -> str:
         logging.debug(f"Match detected in CNAME '{match} for domain '{domain}'")
         return True
     return False
+
+
+def NX_DOMAIN_on_resolve(domain: Domain) -> bool:
+    for cname in domain.CNAME:
+        cname = Domain(cname, fetch_standard_records=True)
+        if cname.A != []:
+            logging.debug(f"A records returned for cname {cname}")
+            continue
+        if cname.AAAA != []:
+            logging.debug(f"AAAA records returned for cname {cname}")
+            continue
+        if cname.CNAME != []:
+            logging.debug(f"NS records returned for cname {cname}")
+            continue
+        if cname.NS != []:
+            logging.debug(f"NS records returned for cname {cname}")
+            continue
+        logging.debug(f"No records returned for domain {cname}")
+        return True
+    return False
