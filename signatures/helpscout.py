cfe4a268eb2f87b4bdfa81e67f6990cdd950ddd0
charles varnham <charles.varnham@punksecurity.co.uk>
2022-07-22 16:47:21 +0100

    feat: added signature for helpscout

---
+++ signatures/helpscout.py
@@ -0,0 +1,8 @@
+from .routine.cname_found_but_string_in_body import cname_found_but_string_in_body
+
+test = cname_found_but_string_in_body(
+    cname="cname.helpscoutdocs.com",
+    domain_not_configured_message="Not Found",
+    service="helpscoutdocs.com",
+    https=True,
+)
