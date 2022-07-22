ad8f88841cdf59edce18a6eb46b493e595d27910
charles varnham <charles.varnham@punksecurity.co.uk>
2022-07-22 15:12:10 +0100

    feat: added signature for jetbrains

---
+++ signatures/jetbrains.py
@@ -0,0 +1,7 @@
+from .routine.cname_found_but_string_in_body import cname_found_but_string_in_body
+
+test = cname_found_but_string_in_body(
+    cname="cname.myjetbrains.com",
+    domain_not_configured_message="YouTrack Starting Page",
+    service="myjetbrains.com",
+)
