b889c7f76a710feb28db4d1b1983c16e6599c9de
Simon Gurney <simon.gurney@punksecurity.co.uk>
2022-07-22 17:10:24 +0100

    feat: add active check for aws

--- tests/signatures_tests/test_aws_ns.py
+++ tests/signatures_tests/test_aws_ns.py
@@ -1,1 +1,8 @@
-# TODO: Active check
+from domain import Domain
+from signatures import aws_ns
+
+
+def test_check_ACTIVE():
+    domain = Domain("mock.local", fetch_standard_records=False)
+    domain.NS = ["ns-40.awsdns-05.com"]
+    assert aws_ns.test.check(domain) == True
