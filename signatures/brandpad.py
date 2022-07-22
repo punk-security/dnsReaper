6f4aba1ad8ab27c1545f8037f6277cdbe579b95f
charles varnham <charles.varnham@punksecurity.co.uk>
2022-07-22 15:30:28 +0100

    feat: added brandpad signature

---
+++ signatures/brandpad.py
@@ -0,0 +1,7 @@
+from .routine.cname_found_but_string_in_body import cname_found_but_string_in_body
+
+test = cname_found_but_string_in_body(
+    cname="cname.brandpad.io",
+    domain_not_configured_message="that doesnt match any whitelabels or custom-domains.",
+    service="brandpad.io",
+)
