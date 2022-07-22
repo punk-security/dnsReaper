6474fe4abf2e8b1beb1a7dd233f69f5fc27ae4b9
charles varnham <charles.varnham@punksecurity.co.uk>
2022-07-22 13:48:19 +0100

    chore: edited cname on ngrok

--- signatures/ngrok.py
+++ signatures/ngrok.py
@@ -5,7 +5,6 @@ test = cname_found_but_string_in_body(
         "cname.us.ngrok.io",
         "cname.eu.ngrok.io",
         "cname.uk.ngrok.io",
-        "abc.cname.us.ngrok.io",
         "cname.ngrok.io",
     ],
     domain_not_configured_message="ERR_NGROK_3200",
