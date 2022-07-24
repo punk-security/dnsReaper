be54d8777d63afbcb1668b39e15ba8cd4e87a4ea
Simon Gurney <simon.gurney@punksecurity.co.uk>
2022-07-24 16:28:07 +0100

    feat: add active check

--- tests/signatures_tests/test_cname_found_doesnt_resolve.py
+++ tests/signatures_tests/test_cname_found_doesnt_resolve.py
@@ -1,6 +1,7 @@
 from domain import Domain
 from signatures import cname_found_doesnt_resolve
-from unittest.mock import patch, PropertyMock
+from unittest.mock import patch
+from tests import mocks
 
 
 def test_potential_success_with_a_cname():
@@ -26,3 +27,11 @@ def test_check_failure():
     domain.CNAME = ["cname"]
     with patch("domain.Domain.query", return_value=["something"]):
         assert cname_found_doesnt_resolve.test.check(domain) == False
+
+
+def test_check_success_ACTIVE():
+    test_cname = f"{mocks.random_string()}.io"
+    domain = Domain(f"{mocks.random_string()}.com", fetch_standard_records=False)
+    domain.CNAME = [test_cname]
+    print(f"Testing cname {test_cname}")
+    assert cname_found_doesnt_resolve.test.check(domain) == True
