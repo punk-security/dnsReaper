b76cd1bfc6ef7fb3ad52b02bd86402c88deffae1
SimonGurney <simon.gurney@punksecurity.co.uk>
2022-07-25 12:48:26 +0100

    feat: remove yahoo sig
    Yahoo is now owned by verizon so doubt this is a go-er

--- signatures/yahoo_small_buissness.py
+++
@@ -1,9 +0,0 @@
-from .templates.ns_found_but_no_SOA import ns_found_but_no_SOA
-
-test = ns_found_but_no_SOA(
-    [
-        "yns1.yahoo.com",
-        "yns2.yahoo.com",
-    ],
-    service="yahoo.com",
-)
