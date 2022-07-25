3c22b5da6e8bad6d443e3a493a2187806910006e
charles varnham <charles.varnham@punksecurity.co.uk>
2022-07-25 11:31:35 +0100

    bug: fixed wishbond domain config message

--- signatures/wishpond.py
+++ signatures/wishpond.py
@@ -2,6 +2,6 @@ from .templates.cname_found_but_string_in_body import cname_found_but_string_in_
 
 test = cname_found_but_string_in_body(
     cname="cname.wishpond.com",
-    domain_not_configured_message="404",
+    domain_not_configured_message="https://www.wishpond.com/404?campaign=true",
     service="wishpond.com",
 )
