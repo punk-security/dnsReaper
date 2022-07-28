5c3e2737f5e182fa265b15a95b3c002a167e3b87
Simon Gurney <simon.gurney@punksecurity.co.uk>
2022-07-28 20:19:26 +0100

    fix: correct ns for name.com

--- signatures/name.py
+++ signatures/name.py
@@ -1,3 +1,5 @@
 from .templates.ns_found_but_no_SOA import ns_found_but_no_SOA
 
-test = ns_found_but_no_SOA(ns="name", service="name.com", sample_ns="ns3nrz.name.com")
+test = ns_found_but_no_SOA(
+    ns=".name.com", service="name.com", sample_ns="ns3nrz.name.com"
+)
