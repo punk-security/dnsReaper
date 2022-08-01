996228fbd77399654874dfbe605db16eb66985a1
charles varnham <charles.varnham@punksecurity.co.uk>
2022-08-01 10:27:35 +0100

    chore: changed campaign monitor cname

--- signatures/campaign_monitor.py
+++ signatures/campaign_monitor.py
@@ -1,7 +1,7 @@
 from .templates.cname_found_but_string_in_body import cname_found_but_string_in_body
 
 test = cname_found_but_string_in_body(
-    cname="goose.campaignmonitor.com",
+    cname="cname.campaignmonitor.com",
     domain_not_configured_message="WHOOPS! The page you're looking for does not exist.",
     service="campaignmonitor.com",
 )
