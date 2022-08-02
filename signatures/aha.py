144af4d21385391934658a50de0392728a4ba1c4
charles varnham <charles.varnham@punksecurity.co.uk>
2022-08-02 10:10:42 +0100

    feat: added signature for aha

---
+++ signatures/aha.py
@@ -0,0 +1,7 @@
+from .templates.cname_found_but_string_in_body import cname_found_but_string_in_body
+
+test = cname_found_but_string_in_body(
+    cname=".aha.io",
+    domain_not_configured_message="Unable to load ideas portal",
+    service="aha.io",
+)
