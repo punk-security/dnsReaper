50621683776b7574a590f8775ac8c8e4f4d085ea
charles varnham <charles.varnham@punksecurity.co.uk>
2022-08-02 10:02:06 +0100

    feature: aha signature

--- signatures/aha.py
+++ signatures/aha.py
@@ -1,7 +1,7 @@
 from .templates.cname_found_but_string_in_body import cname_found_but_string_in_body
 
 test = cname_found_but_string_in_body(
-    cname=".aha.io",
+    cname="cname.aha.io",
     domain_not_configured_message="Unable to load ideas portal",
     service="aha.io",
 )
