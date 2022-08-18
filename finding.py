2ec565d6945dac4a848614bca4b08a43453c4c64
Simon Gurney <simon.gurney@punksecurity.co.uk>
2022-08-18 16:15:15 +0100

    bug: missed commit from #107

--- finding.py
+++ finding.py
@@ -1,6 +1,31 @@
+from os import linesep
+
+
 class Finding(object):
-    def __init__(self, domain, signature, info, confidence):
-        self.domain = domain
+    def __init__(
+        self,
+        domain,
+        signature,
+        info,
+        confidence,
+    ):
+        self.domain = domain.domain
         self.signature = signature
         self.info = info.replace("\n", " ").replace("\r", "").rstrip()
         self.confidence = confidence.name
+        self.a_records = domain.A
+        self.aaaa_records = domain.AAAA
+        self.cname_records = domain.CNAME
+        self.ns_records = domain.NS
+
+    def populated_records(self):
+        resp = ""
+        if self.a_records:
+            resp += f"A: {self.a_records},"
+        if self.aaaa_records:
+            resp += f"AAAA: {self.aaaa_records},"
+        if self.cname_records:
+            resp += f"CNAME: {self.cname_records},"
+        if self.ns_records:
+            resp += f"NS: {self.ns_records},"
+        return resp.rstrip(",")
