0879e42abac44fd6be4cd83e0719c0e3c973aa37
CharlesVarnham <70818396+noopec@users.noreply.github.com>
2022-08-25 12:18:16 +0100

    Delete cargo_collective.py

--- signatures/cargo_collective.py
+++
@@ -1,7 +0,0 @@
-from .templates.cname_found_but_string_in_body import cname_found_but_string_in_body
-
-test = cname_found_but_string_in_body(
-    cname="cname.cargo.site",
-    domain_not_configured_message="Should you require further assistance, please",
-    service="Cargo Collective",
-)
