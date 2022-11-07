d2bf331fce54887b4058f816cd393738663eaf32
Simon Gurney <simon.gurney@punksecurity.co.uk>
2022-11-07 09:16:08 +0000

    fix: switch launchrock to code check

--- signatures/launchrock_cname.py
+++ signatures/launchrock_cname.py
@@ -1,8 +1,8 @@
-from .templates.cname_found_but_string_in_body import cname_found_but_string_in_body
+from .templates.cname_found_but_status_code import cname_found_but_status_code
 
-test = cname_found_but_string_in_body(
+test = cname_found_but_status_code(
     cname="cname.launchrock.com",
-    domain_not_configured_message="It looks like you may have taken a wrong turn somewhere.",
+    code=500,
     service="Launchrock",
     https=True,
 )
