5c6066fbb7ad15931cd1d5023291bfffc84a8ef9
Victoria Kotiwcki <victoria.kotwicki@punksecurity.co.uk>
2023-07-20 17:59:37 +0100

    feat: added google cloud

--- providers/googlecloud.py
+++ providers/googlecloud.py
@@ -1,171 +1,84 @@
-import requests
+import logging, os
 
 from domain import Domain
 from google.cloud import dns
-from google.cloud.client import ClientWithProject
-from google.cloud.exceptions import NotFound
 
+description = "Scan multiple domains by fetching them from Google Cloud. Requires GOOGLE_APPLICATION_CREDENTIALS environment variable."
 
-#TODO: auth
+def get_records(zone):
+    records = []    
 
-description = "Scan multiple domains by fetching them from Google Cloud"
-
-
-class DomainNotFoundError(Exception):
-    def __init__(self, domain):
-        self.message = "Domain not found: " + domain
-        super().__init__(self.message)
-
-
-class DoApi:
-    def __init__(self, api_key):
-        self.session = requests.session()
-        self.session.headers.update(
-            {"Content-Type": "application/json", "Authorization": "Bearer " + api_key}
+    try:
+        records = zone.list_resource_record_sets(max_results=None, page_token=None)
+    except Exception as e:
+        logging.critical(
+            f"Failed to fetch zone records. {e}"
         )
+        return []
 
-    def auth_client(
-        project=None, # Opt
-        credentials=None, # Opt, Creditals class
-        _http=None, 
-        client_options=None
-    ):
-        client = dns.Client()
+    return list(records)
 
 
-    @staticmethod
-    def check_response(response: requests.Response):
-        if response.status_code == 401:
-            raise ValueError("Invalid API key specified.")
-
-        if response.status_code < 200 or response.status_code >= 300:
-            raise ValueError("Invalid response received from API: " + response.json())
-
-        return response
-
-    def make_request(self, endpoint):
-        return self.session.prepare_request(
-            requests.Request("GET", "https://api.digitalocean.com/v2/" + endpoint)
-        )
-
-    def list_domains(self):
-        req = self.make_request("domains")
-
-        return self.check_response(self.session.send(req))
-
-    def get_records(self, domain):
-        req = self.make_request(f"domains/{domain}/records")
-        res = self.session.send(req)
-
-        if 404 == res.status_code:
-            raise DomainNotFoundError(domain)
-
-        return self.check_response(res)
-    
-    ###
-    def get_zone(project_id, name):
-        client = dns.Client(project=project_id)
-        zone = client.zone(name=name)
-
-        try:
-            zone.reload()
-            return zone
-        except NotFound:
-            return None
-        
-    def list_zones(project_id):
-        client = dns.Client(project=project_id)
-        zones = client.list_zones()
-        return [zone.name for zone in zones]
-    
-    def list_resource_records(project_id, zone_name):
-        client = dns.Client(project=project_id)
-        zone = client.zone(zone_name)
-
-        records = zone.list_resource_record_sets()
-
-        return [
-            (record.name, record.record_type, record.ttl, record.rrdatas)
-            for record in records
-        ]
-    
-    def get_command(args):
-        """Gets a zone by name."""
-        zone = get_zone(args.project_id, args.name)
-        if not zone:
-            print("Zone not found.")
-        else:
-            print("Zone: {}, {}, {}".format(zone.name, zone.dns_name, zone.description))
-
-
-def convert_records_to_domains(records, root_domain):
+def convert_records_to_domains(records):
     buf = {}
     for record in records:
-        if "@" == record["name"]:
-            continue
+        if record.name not in buf.keys():
+            buf[record.name] = {}
 
-        record_name = f"{record['name']}.{root_domain}"
-
-        if record_name not in buf.keys():
-            buf[record_name] = {}
-
-        if record["type"] not in buf[record_name].keys():
-            buf[record_name][record["type"]] = []
-
-        if "data" in record.keys():
-            buf[record_name][record["type"]].append(record["data"])
-
-    def extract_records(desired_type):
-        return [r.rstrip(".") for r in buf[subdomain][desired_type]]
+        buf[record.name][record.record_type] = record.rrdatas
 
     for subdomain in buf.keys():
         domain = Domain(subdomain.rstrip("."), fetch_standard_records=False)
-
         if "A" in buf[subdomain].keys():
-            domain.A = extract_records("A")
+            domain.A = [r.rstrip(".") for r in buf[subdomain]["A"]]
         if "AAAA" in buf[subdomain].keys():
-            domain.AAAA = extract_records("AAAA")
+            domain.AAAA = [r.rstrip(".") for r in buf[subdomain]["AAAA"]]
         if "CNAME" in buf[subdomain].keys():
-            domain.CNAME = extract_records("CNAME")
+            domain.CNAME = [r.rstrip(".") for r in buf[subdomain]["CNAME"]]
         if "NS" in buf[subdomain].keys():
-            domain.NS = extract_records("NS")
-
+            domain.NS = [r.rstrip(".") for r in buf[subdomain]["NS"]]
         yield domain
 
 
-def validate_args(gc_api_key: str):
-    if not gc_api_key.startswith("dop_v1"):
-        raise ValueError("Google Cloud: Invalid API key specified")
+def get_zones(client):
+    zones = []
+    try:
+        zones = client.list_zones()
+        zones = list(zones)
+    except Exception as e:
+        logging.critical(
+            f"""Failed to fetch zones from Google Cloud. Could not discover credentials.
+Ensure that the environment variable `GOOGLE_APPLICATION_CREDENTIALS` is set to the JSON credential file's location, 
+or that the JSON file is in the default location. 
+{e}"""
+        )
+        exit(-1)
+
+    logging.debug(f"Got {len(zones)} zones from Google Cloud")
+    if len(zones) == 0:
+        return []
     
-def grant_auth(gc_client_id: str):
-    authParams = {
-        'response_type' : 'token', 
-        'client_id' : gc_client_id+'.apps.googleusercontent.com',
-        'immediate' : True,
-        'scope' : ['https://www.googleapis.com/auth/ndev.clouddns.readonly']
-    }
+    return zones
 
-dns.
-
-
-def fetch_domains(project: str, gc_client_id: str, gc_secret: str, gc_domains: str = None, **args):  # NOSONAR
-    grant_auth(gc_client_id)
-    validate_args(gc_secret)
-    root_domains = []
+def fetch_domains(project_id, **args):
     domains = []
-    api = DoApi(gc_secret)
 
-    if gc_domains is not None and len(gc_domains):
-        root_domains = [domain.strip(" ") for domain in gc_domains.split(",")]
-    else:
-        resp_data = api.list_domains().json()
-        root_domains = [domain["name"] for domain in resp_data["domains"]]
+    logger = logging.getLogger("google")
+    logger.setLevel(logging.CRITICAL)
 
-    for domain in root_domains:
-        if "" == domain or domain is None:
-            continue
+    client = dns.Client(project_id)
 
-        records = api.get_records(domain).json()
-        domains.extend(convert_records_to_domains(records["domain_records"], domain))
+    zones = get_zones(client)
 
+    for zone in zones:
+        try:
+            records = get_records(zone)
+        except:
+            logging.warning(f"Could not retrieve records for Google Cloud zone '{zone.dns_name}'")
+            records = []
+        logging.debug(f"Got {len(records)} records for Google Cloud zone '{zone.dns_name}'")
+
+        for domain in convert_records_to_domains(records):
+            domains.append(domain)
+    logging.warning(f"Got {len(domains)} records from Google Cloud")
     return domains
