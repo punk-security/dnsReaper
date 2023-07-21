f1d68345dce3af5966d25ca8e85e3659e4f53f7a
Victoria Kotiwcki <victoria.kotwicki@punksecurity.co.uk>
2023-07-21 10:50:23 +0100

    feat: security trails

--- providers/securitytrails.py
+++ providers/securitytrails.py
@@ -10,12 +10,15 @@ class DomainNotFoundError(Exception):
         self.message = "Domain not found: " + domain
         super().__init__(self.message)
 
+
+
 class STApi:
     def __init__(self, api_key):
         self.session = requests.session()
         self.session.headers.update(
-            {"Content-Type": "application/json", "Authorization": api_key}
+            {"accept": "application/json", "APIKEY": api_key}
         )
+
 
     @staticmethod
     def check_response(response: requests.Response):
@@ -27,18 +30,21 @@ class STApi:
 
         return response
     
+    
     def make_request(self, endpoint):
         return self.session.prepare_request(
-            requests.Request("GET", "https://dns.projectdiscovery.io/dns/" + endpoint)
+            requests.Request("GET", "https://api.securitytrails.com/v1/" + endpoint)
         )
+
     
     def list_domains(self):
-        req = self.make_request("domains")
+        req = self.make_request("domains/list?include_ips=false")
 
         return self.check_response(self.session.send(req))
+    
 
     def get_subdomains(self, domain):
-        req = self.make_request(f"{domain}/subdomains")
+        req = self.make_request(f"domain/{domain}/subdomains")
         res = self.session.send(req)
 
         if 404 == res.status_code:
@@ -46,23 +52,22 @@ class STApi:
 
         return self.check_response(res)
 
-def fetch_domains(st_api_key: str, st_domains: str = None, **args):
+
+def fetch_domains(st_api_key: str, st_domains: str, **args):
     root_domains = []
     domains = []
     api = STApi(st_api_key)
 
-    if st_domains is not None and len(st_domains):
-        root_domains = [domain.strip(" ") for domain in st_domains.split(",")]
-    else:
-        print("Domain required")
-        exit()
+    root_domains = [domain.strip(" ") for domain in st_domains.split(",")]
 
     for domain in root_domains:
         if "" == domain or domain is None:
             continue
 
         raw_domains = api.get_subdomains(domain).json()
-        logging.info("Testing", len(raw_domains["subdomains"]), "subdomains")
+
+        logging.info("Testing", raw_domains["subdomain_count"], "subdomains")
         domains.extend([Domain(f"{sb}.{domain}") for sb in raw_domains["subdomains"]])
+        print(domains)
 
     return domains
