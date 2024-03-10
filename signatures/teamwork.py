68b59404256a2a374c2d1dc7777fca4e0f393fd5
Simon Gurney <simon.gurney@punksecurity.co.uk>
2024-03-10 13:58:04 +0000

    feat: add custom uri on body checks

--- signatures/teamwork.py
+++ signatures/teamwork.py
@@ -2,6 +2,8 @@ from .templates.cname_found_but_string_in_body import cname_found_but_string_in_
 
 test = cname_found_but_string_in_body(
     cname=".teamwork.com",
-    domain_not_configured_message="""<title>Teamwork Projects</title>""",
+    domain_not_configured_message="""Unable to determine installationID from domain""",
     service="teamwork",
+    custom_uri="launchpad/v1/info.json",
+    https=True
 )
--- signatures/templates/ip_found_but_string_in_body.py
+++ signatures/templates/ip_found_but_string_in_body.py
@@ -19,10 +19,10 @@ class ip_found_but_string_in_body(base.Base):
     def check(self, domain, **kwargs) -> bool:
         if self.https:
             return signatures.checks.WEB.string_in_body_https(
-                domain, self.domain_not_configured_message
+                domain, self.domain_not_configured_message, custom_uri=self.custom_uri
             )
         return signatures.checks.WEB.string_in_body_http(
-            domain, self.domain_not_configured_message
+            domain, self.domain_not_configured_message, custom_uri=self.custom_uri
         )
 
     def __init__(
@@ -32,10 +32,12 @@ class ip_found_but_string_in_body(base.Base):
         service,
         info=None,
         https=False,
+        custom_uri="",
         **kwargs
     ):
         self.ips = ips
         self.domain_not_configured_message = domain_not_configured_message
         self.https = https
+        self.custom_uri = custom_uri
         info = info if info else INFO
         super().__init__(info.format(service=service), **kwargs)
