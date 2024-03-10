87be3363dd3171af9f6d21ea31c48250e54d1330
SimonGurney <simon.gurney@punksecurity.co.uk>
2024-03-10 14:03:32 +0000

    feat: add custom uri on body checks (#172)
    * feat: add custom uri on body checks
    
    * chore: black

--- signatures/checks/WEB.py
+++ signatures/checks/WEB.py
@@ -3,20 +3,22 @@ from math import floor
 import logging
 
 
-def string_in_body(domain: Domain, string: str, https: bool) -> bool:
-    if string in domain.fetch_web(https=https).body:
+def string_in_body(
+    domain: Domain, string: str, https: bool, custom_uri: str = ""
+) -> bool:
+    if string in domain.fetch_web(https=https, uri=custom_uri).body:
         logging.info(f"Message observed in response for '{domain}'")
         return True
     logging.debug(f"Message not found in response for '{domain}'")
     return False
 
 
-def string_in_body_http(domain: Domain, string: str) -> bool:
-    return string_in_body(domain, string, False)
+def string_in_body_http(domain: Domain, string: str, custom_uri: str = "") -> bool:
+    return string_in_body(domain, string, False, custom_uri)
 
 
-def string_in_body_https(domain: Domain, string: str) -> bool:
-    return string_in_body(domain, string, True)
+def string_in_body_https(domain: Domain, string: str, custom_uri: str = "") -> bool:
+    return string_in_body(domain, string, True, custom_uri)
 
 
 def status_code_match(domain: Domain, status_code: int, https: bool) -> bool:
--- signatures/templates/cname_or_ip_found_but_string_in_body.py
+++ signatures/templates/cname_or_ip_found_but_string_in_body.py
@@ -19,10 +19,10 @@ class cname_or_ip_found_but_string_in_body(base.Base):
     def check(self, domain, **kwargs) -> bool:
         if self.https:
             return signatures.checks.WEB.string_in_body_https(
-                domain, self.domain_not_configured_message
+                domain, self.domain_not_configured_message, custom_uri=self.custom_uri
             )
         return signatures.checks.WEB.string_in_body_http(
-            domain, self.domain_not_configured_message
+            domain, self.domain_not_configured_message, custom_uri=self.custom_uri
         )
 
     def __init__(
@@ -33,11 +33,13 @@ class cname_or_ip_found_but_string_in_body(base.Base):
         service,
         info=None,
         https=False,
+        custom_uri="",
         **kwargs
     ):
         self.cname = cname
         self.ips = ips
         self.domain_not_configured_message = domain_not_configured_message
         self.https = https
+        self.custom_uri = custom_uri
         info = info if info else INFO
         super().__init__(info.format(service=service), **kwargs)
