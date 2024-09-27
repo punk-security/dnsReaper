44dc210cc2a22b7be9d2b726504d1b15e1735c2b
Simon Gurney <simon.gurney@punksecurity.co.uk>
2024-09-27 23:24:29 +0100

    chore: fix sigs

--- signatures/brandpad.py
+++ signatures/brandpad.py
@@ -1,7 +1,7 @@
 from .templates.cname_found_but_string_in_body import cname_found_but_string_in_body
 
 test = cname_found_but_string_in_body(
-    cname="cname.brandpad.io",
+    cname="brandpad.io",
     domain_not_configured_message="is not registered as whitelabel or custom domain.",
     service="brandpad.io",
 )
